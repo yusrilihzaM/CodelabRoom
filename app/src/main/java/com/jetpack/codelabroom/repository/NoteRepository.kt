6e1c90a5710b076a24e1a507f9602a36ca5bbc18
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-06-02 20:28:31 +0700

    Initial commit

---
+++ app/src/main/java/com/jetpack/codelabroom/repository/NoteRepository.kt
@@ -0,0 +1,29 @@
+package com.jetpack.codelabroom.repository
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+import com.jetpack.codelabroom.database.Note
+import com.jetpack.codelabroom.database.NoteDao
+import com.jetpack.codelabroom.database.NoteRoomDatabase
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+class NoteRepository(application: Application) {
+    private val mNotesDao: NoteDao
+    private val executorService: ExecutorService = Executors.newSingleThreadExecutor()
+    init {
+        val db = NoteRoomDatabase.getDatabase(application)
+        mNotesDao = db.noteDao()
+    }
+    fun getAllNotes(): LiveData<List<Note>> = mNotesDao.getAllNotes()
+
+    fun insert(note: Note) {
+        executorService.execute { mNotesDao.insert(note) }
+    }
+    fun delete(note: Note) {
+        executorService.execute { mNotesDao.delete(note) }
+    }
+    fun update(note: Note) {
+        executorService.execute { mNotesDao.update(note) }
+    }
+}
---
+++ app/src/main/java/com/jetpack/codelabroom/ui/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jetpack.codelabroom.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jetpack.codelabroom.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
